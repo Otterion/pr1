0f63b78f0a41d28f56f904c8c1acbf3b76941a54
Otterion <92106428+Otterion@users.noreply.github.com>
2021-11-05 10:23:25 +0300

    Initial commit

---
+++ src/com/company/Animal.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface Animal {
+    int legs = 0;
+    void say();
+    void canSwim();
+    void canRun();
+    void canFly();
+}
---
+++ src/com/company/Fish.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Fish implements Animal{
+    @Override
+    public void say() {
+        System.out.println("");
+    }
+
+    @Override
+    public void canSwim() {
+
+    }
+
+    @Override
+    public void canRun() {
+
+    }
+
+    @Override
+    public void canFly() {
+
+    }
+}
